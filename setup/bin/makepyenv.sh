e1455d47aceaff36c83415ca7ff2f1a286a125f0
peristrophe <peristrophe@live.jp>
2018-04-03 11:05:52 +0900

    create script for python env building.

---
+++ setup/bin/makepyenv.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+[ -z $(which pyenv) ] && echo 'command not found: pyenv' >&2 && exit 1
+
+python_version=$(pyenv install -l | awk '{print $1}' | grep -E '^\d\.\d\.\d$' | tail -1)
+pyenv install ${python_version}
+pyenv global ${python_version}
+pip install ../etc/pip-packages.txt
+pip list -o | awk '{print $1}' | xargs pip install -U
+pip freeze > ../etc/pip-packages.txt
+pyenv global system
