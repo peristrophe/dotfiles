22c5e76d373fe1c4c9d884468f5057e1695bc8f7
peristrophe <peristrophe@live.jp>
2018-04-03 18:56:33 +0900

    modify script

--- setup/bin/makepyenv.sh
+++ setup/bin/makepyenv.sh
@@ -5,7 +5,7 @@
 python_version=$(pyenv install -l | awk '{print $1}' | grep -E '^\d*\.\d*\.\d*$' | tail -1)
 pyenv install ${python_version}
 pyenv global ${python_version}
-pip install ../etc/pip-packages.txt
-pip list -o | awk '{print $1}' | xargs pip install -U
+pip install -U ../etc/pip-packages.txt
+#pip list -o | awk '{print $1}' | xargs pip install -U
 pip freeze > ../etc/pip-packages.txt
 pyenv global system
