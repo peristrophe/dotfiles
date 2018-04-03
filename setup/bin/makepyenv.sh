dd770dbb5ce60a197d0bfef862d054467efe304f
peristrophe <peristrophe@live.jp>
2018-04-03 11:10:07 +0900

    fix regular expression.

--- setup/bin/makepyenv.sh
+++ setup/bin/makepyenv.sh
@@ -2,7 +2,7 @@
 
 [ -z $(which pyenv) ] && echo 'command not found: pyenv' >&2 && exit 1
 
-python_version=$(pyenv install -l | awk '{print $1}' | grep -E '^\d\.\d\.\d$' | tail -1)
+python_version=$(pyenv install -l | awk '{print $1}' | grep -E '^\d*\.\d*\.\d*$' | tail -1)
 pyenv install ${python_version}
 pyenv global ${python_version}
 pip install ../etc/pip-packages.txt
