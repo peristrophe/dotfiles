7abf7fed17c1500720c7b16565d0043fe2d228ad
peristrophe <peristrophe@live.jp>
2018-04-10 13:13:09 +0900

    test

--- setup/bin/makeEnv-homebrew.sh
+++ setup/bin/makeEnv-homebrew.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
+here=$(cd $(dirname $0); pwd)
+. ${here}/settings.sh
+
 xcode-select --install
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
