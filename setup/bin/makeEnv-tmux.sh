708ca5f8d6c8706fe717698845a7e3f9b75dcbb6
peristrophe <peristrophe@live.jp>
2018-04-13 12:40:29 +0900

    Adjustment statusline visual

--- setup/bin/makeEnv-tmux.sh
+++ setup/bin/makeEnv-tmux.sh
@@ -19,7 +19,24 @@ else
     git clone https://github.com/powerline/fontpatcher.git ${GITDIR}/fontpatcher
 fi
 
+[ -f ~/Downloads/NasuFonts.zip ] && rm -f ~/Downloads/NasuFonts.zip
+[ -d ~/Downloads/NasuFonts ] && rm -rf ~/Downloads/NasuFonts
+
+if [ $(which wget) ];
+then
+    wget 'https://ja.osdn.net/frs/chamber_redir.php?m=jaist&f=%2Fusers%2F7%2F7587%2FNasuFont-20141215.zip' -O ~/Downloads/NasuFonts.zip
+elif [ $(which curl) ];
+then
+    curl -L 'https://ja.osdn.net/frs/chamber_redir.php?m=jaist&f=%2Fusers%2F7%2F7587%2FNasuFont-20141215.zip' -o ~/Downloads/NasuFonts.zip
+else
+    exit 1
+fi
+
+mkdir -p ~/Downloads/NasuFonts
+unzip -j ~/Downloads/NasuFonts.zip "*.ttf" -d ~/Downloads/NasuFonts
+
 cp "${BINDIR}/powerline-fontpatcher-for-NasuM.py" "${GITDIR}/fontpatcher/scripts/"
 cd "${GITDIR}/fontpatcher/scripts"
-fontforge -lang=py -script "${GITDIR}/fontpatcher/scripts/powerline-fontpatcher-for-NasuM.py" ~/Library/Fonts/NasuM-*.ttf
+fontforge -lang=py -script "${GITDIR}/fontpatcher/scripts/powerline-fontpatcher-for-NasuM.py" ~/Downloads/NasuFonts/NasuM-*.ttf
 
+cp ${GITDIR}/fontpatcher/scripts/NasuM-*.ttf ~/Library/Fonts/
