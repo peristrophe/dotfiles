ccc83a335ee1162f80e00712642f6a4d6aca4a55
peristrophe <peristrophe@live.jp>
2018-05-10 10:28:57 +0900

    Apply 2018-05-10

--- setup/bin/makeEnv-tmux.sh
+++ setup/bin/makeEnv-tmux.sh
@@ -3,13 +3,13 @@
 here=$(cd $(dirname $0); pwd)
 . ${here}/settings.sh
 
-if [ -d "${GITDIR}/fonts" ];
-then
-    cd "${GITDIR}/fonts"
-    git pull
-else
-    git clone https://github.com/powerline/fonts.git ${GITDIR}/fonts
-fi
+#if [ -d "${GITDIR}/fonts" ];
+#then
+#    cd "${GITDIR}/fonts"
+#    git pull
+#else
+#    git clone https://github.com/powerline/fonts.git ${GITDIR}/fonts
+#fi
 
 if [ -d "${GITDIR}/fontpatcher" ];
 then
