3bbd900d814ae11f8d5abf6e51f77672564f93ff
peristrophe <peristrophe@live.jp>
2023-02-12 20:44:10 +0900

    update install-terminal-font.sh

--- setup/bin/install-terminal-font.sh
+++ setup/bin/install-terminal-font.sh
@@ -14,12 +14,12 @@ trap sweep 0 1 2 3 15
 
 
 case $1 in
-    martian-mono)
+    martian-mono | MartianMono)
         url='https://github.com/evilmartians/mono/releases/download/v1.0.0/martian-mono-1.0.0-ttf.zip'
         prefix='MartianMono'
         extension='ttf'
         ;;
-    hack-gen)
+    hack-gen | HackGen)
         url='https://github.com/yuru7/HackGen/releases/download/v2.8.0/HackGen_NF_v2.8.0.zip'
         prefix='HackGen'
         extension='ttf'
